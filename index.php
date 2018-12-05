144a023dab8735cb5160d82977809cd0ce0f5e59
christian <christian.kramer@mail.com>
2018-12-05 14:41:14 -0600

    moved credentials to separate file

--- index.php
+++ index.php
@@ -4,10 +4,11 @@ error_reporting(E_ALL); ini_set('display_errors', 1);
 
 require "./oauth2-lib.php";
 
+$credentials = json_decode(file_get_contents("credentials"));
 
 $discord_oauth = new OAuth2Service([
-    'clientId'                => '',    // The client ID assigned to you by the provider
-    'clientSecret'            => '',   // The client password assigned to you by the provider
+    'clientId'                => $credentials->clientId,    // The client ID assigned to you by the provider
+    'clientSecret'            => $credentials->clientSecret,   // The client password assigned to you by the provider
     'redirectUri'             => 'http://dev.api.nerdev.io/index.php',
     'urlBase'                 => 'https://discordapp.com/api',
     'urlAuthorize'            => 'https://discordapp.com/api/oauth2/authorize',
