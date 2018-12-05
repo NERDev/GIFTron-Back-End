e47c58447bdf1ef2ad96d57241ef7a6b9fce9213
christian <christian.kramer@mail.com>
2018-12-05 16:24:06 -0600

    created DiscordAPI class with OAuth2Service inheritance

--- index.php
+++ index.php
@@ -2,18 +2,24 @@
 
 error_reporting(E_ALL); ini_set('display_errors', 1);
 
-require "./oauth2-lib.php";
+require "discord-lib.php";
+
+
+
+
+
+
+
+
+
 
 $credentials = json_decode(file_get_contents("credentials"));
 
-$discord_oauth = new OAuth2Service([
-    'clientId'                => $credentials->clientId,    // The client ID assigned to you by the provider
-    'clientSecret'            => $credentials->clientSecret,   // The client password assigned to you by the provider
-    'redirectUri'             => 'http://dev.api.nerdev.io/index.php',
-    'urlBase'                 => 'https://discordapp.com/api',
-    'urlAuthorize'            => 'https://discordapp.com/api/oauth2/authorize',
-    'urlAccessToken'          => 'https://discordapp.com/api/oauth2/token'
-]);
+$discordAPI = new DiscordAPI($credentials->clientId, $credentials->clientSecret);
 
-var_dump($discord_oauth->getAccessToken('authorization_code', $_GET['code']));
-var_dump($discord_oauth->getUserInfo());
+
+
+
+var_dump($discordAPI->getAccessToken('authorization_code', $_GET['code']));
+
+var_dump($discordAPI->getUserInfo());
