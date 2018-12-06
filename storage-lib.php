d239ed4f9bbc351d191f266b63efacdf213d15e4
christian <christian.kramer@mail.com>
2018-12-05 21:20:05 -0600

    cursory outline of storage-lib

---
+++ storage-lib.php
@@ -0,0 +1,50 @@
+<?php
+
+
+class StorageClient extends Storage
+{
+    
+}
+
+class StorageServer extends Storage
+{
+    private function partner()
+    {
+        //choose a partner to store a backup
+    }
+
+    function read()
+    {
+        //read from local storage and compare to remote
+        //if different, overwrite whichever is older with whichever is newer
+    }
+
+    function write()
+    {
+        //write to local and remote storage
+    }
+}
+
+class Storage
+{
+    protected $basedir = "../data";
+
+    function __construct()
+    {
+
+    }
+
+    protected function local_read($query)
+    {
+        json_decode(file_get_contents("$this->basedir/$query"));
+    }
+
+    protected function local_write($query, $data)
+    {
+        $path = explode($query);
+        array_pop($path);
+        $parentdir = implode('/', $path);
+        mkdir($parentdir, 0755, TRUE);
+        file_put_contents("$query", json_encode($data));
+    }
+}
