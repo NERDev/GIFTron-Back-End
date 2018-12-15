bf48b3e0f4fd45aea8494967ebf39304475f8939
christian <christian.kramer@mail.com>
2018-12-15 12:40:06 -0600

    working storage read chain

--- v1/libraries/storage-lib.php
+++ v1/libraries/storage-lib.php
@@ -1,7 +1,7 @@
 <?php
 
-define('HERE', array_shift(explode('.', gethostname())));
-define('ALPHABET', array_diff([HERE], range('a', 'z')));
+define('HERE', array_shift(explode('.', $_SERVER['SERVER_NAME'])));
+define('ALPHABET', range('a', 'z'));
 
 class StorageNode extends Storage
 {
@@ -15,21 +15,25 @@ class StorageNode extends Storage
         $server1 = $location[1];
 
         $primary = HERE == $server0;
-        $secondary = HERE == $secondary;
+        $secondary = HERE == $server1;
 
         if ($primary)
         {
             //get contents of location, compare to secondary
+            var_dump("this is the primary server", file_get_contents("http://$server1.dev.nerdev.io/giftron/api/v1/storage/read?$location"));
         }
 
         if ($secondary)
         {
             //get contents of location
+            var_dump("this is the secondary server");
         }
 
         if (!$secondary && !$primary)
         {
             //this must be the remote server, handle the request to the primary to get the ball rolling
+            var_dump("this is the remote server", file_get_contents("http://$server0.dev.nerdev.io/giftron/api/v1/storage/read?$location"));
+            
         }
     }
 
@@ -45,16 +49,20 @@ class StorageNode extends Storage
         if ($primary)
         {
             //write locally and remotely
+            echo "primary reached";
         }
 
         if ($secondary)
         {
             //write locally
+            echo "secondary reached";
         }
 
         if (!$secondary && !$primary)
         {
             //this must be the remote server, handle the request to the primary to get the ball rolling
+            $partner = $this->partner();
+            echo "chose $partner as partner";
         }
     }
 }
@@ -71,10 +79,10 @@ class Storage
     protected function partner()
     {
         //choose a partner to store a backup
-        $counterfile = ROOT . "/metadata/counter";
-        $count = file_get_contents($counterfile) ?? 0;
+        $counterfile = PHPROOT . "/metadata/counter";
+        $count = intval(file_get_contents($counterfile));
         file_put_contents($counterfile, ++$count);
-        return ALPHABET[$count % (count(ALPHABET) - 1)];
+        return ALPHABET[$count % (count(ALPHABET))];
     }
 
     protected function local_read($query)
