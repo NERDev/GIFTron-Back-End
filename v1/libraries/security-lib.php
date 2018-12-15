75be1f315ef468d2ab6481503661e05ddaddf86f
christian <christian.kramer@mail.com>
2018-12-15 12:39:51 -0600

    globalized phproot, webroot, version

--- v1/libraries/security-lib.php
+++ v1/libraries/security-lib.php
@@ -1,6 +1,11 @@
 <?php
 
 define('ROOT', realpath($_SERVER['DOCUMENT_ROOT'] . '/..'));
+define('PHPROOT', realpath(ROOT . '/git/GIFTron/GIFTron-Back-End'));
+define('WEBROOT', realpath(ROOT . '/webroot'));
+define('VERSION', preg_split('/[\\x5c\/]/', str_replace(ROOT, '', __FILE__))[4]);
+
+//add defines for phproot, webroot, version, and whitelist
 
 class Security
 {
@@ -8,9 +13,12 @@ class Security
 
     function __construct()
     {
-        $this->phproot  = realpath(ROOT . '/git/GIFTron/GIFTron-Back-End');
-        $this->webroot  = realpath(ROOT . '/webroot');
-        $this->version  = preg_split('/[\\x5c\/]/', str_replace(ROOT, '', __FILE__))[4];
+        //$this->phproot  = realpath(ROOT . '/git/GIFTron/GIFTron-Back-End');
+        //$this->webroot  = realpath(ROOT . '/webroot');
+        //$this->version  = preg_split('/[\\x5c\/]/', str_replace(ROOT, '', __FILE__))[4];
+        $this->phproot = PHPROOT;
+        $this->webroot = WEBROOT;
+        $this->version = VERSION;
         $this->whitelist = json_decode(file_get_contents("$this->phproot/metadata/whitelist"));
     }
 
