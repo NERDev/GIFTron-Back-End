4527b6ee39d551bc0df5bf02ae349ee6d4dc26e5
christian <christian.kramer@mail.com>
2018-12-17 17:15:52 -0600

    unified write and read into a single array

--- v1/api.php
+++ v1/api.php
@@ -74,8 +74,8 @@ class APIhost extends Security
         //$this->respond(200, $this->discordAPI->getGuildInfo($_GET['id']));
         //$this->respond(200, $this->discordAPI->getBotGuilds());
         //$this->respond(200, $this->storageAPI->write('ab4281', ["kek" => "stuff"]));
-        $this->storageAPI->write('ab4280', ["kek" => "ayylmao", "things" => ["stuff", "otherstuffs"]]);
-        $this->respond(200, $this->storageAPI->read('ab4280'));
+        ;
+        $this->respond(200, ["write transaction" => $this->storageAPI->write('ab4280', ["kek" => "ayylmao", "things" => ["stuff", "otherstuff"]]), "read transaction" => $this->storageAPI->read('ab4280')]);
     }
 
     function storage_check()
