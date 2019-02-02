15706d1909488d23ddac94f31c471e85185bbd83
christian <christian.kramer@mail.com>
2019-02-02 15:54:23 -0600

    sanizited input

--- bundler.php
+++ bundler.php
@@ -1,8 +1,10 @@
 <?php
 
+//error_reporting(E_ALL); ini_set('display_errors', 1);
+
 $path = realpath("../../git/GIFTron/GIFTron-Front-End");
 
-if (!$qs = $_SERVER['QUERY_STRING'])
+if (!$qs = preg_replace('/\.{2,}/', '', $_SERVER['QUERY_STRING']))
 {
     echo file_get_contents("$path/app.html");
 }
